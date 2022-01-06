8f0a86afea2a125ef98b2a9cd33e96afc0051643
Luwei-Lin <luwei2@ualberta.ca>
2022-01-06 14:32:58 -0600

    Update hello.c file

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void swap(int *a,int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int main(){
+    int a = 8;
+    int b = 6;
+    printf("a = %d, b = %d\n", a, b);
+    swap(&a, &b);
+    printf("after swapping...\na = %d, b = %d\n", a, b);
+    printf("hello world\n");
+}
+
